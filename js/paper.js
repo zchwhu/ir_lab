221b3181a4ecdc21b5c1db45b6e4e5c3b5f824f9
zchwhu <291053953@qq.com>
2017-06-18 19:29:02 +0800

    @dev Paper page entry script

--- js/paper.js
+++ js/paper.js
@@ -7,13 +7,39 @@ requirejs.config({
     }
 });
 
-require(['jquery','js/util/scrollChange.js'], function ($,scrollChange) {
+require(['jquery', 'js/util/scrollChange.js', 'js/util/setFixed.js','js/util/toggleShow.js'], function ($, scrollChange, setFixed,toggleShow) {
+    var CONST = {
+        SCROLL_Y: 130,
+        MAIN_WINDTH: 1140,
+        NAV_HEIGHT: 100,
+        BANNER_HEIGHT: 380
+    }
+
+    toggleShow('.ir-user-logged','.ir-dropdown','show');
+
     scrollChange({
         container: '.ir-paper>.ir-container',
         sidebar: '.ir-paper-year-list',
         menus: '.ir-paper-year-item',
         contentList: '.ir-paper-sub-area',
-        coverHeight: 130,
-        fixedTop: 130
+        coverHeight: CONST.SCROLL_Y,
+        speed: 1000
+    })
+
+    var $winWidth = $(window).width(),
+        $setFixedLeft = ($winWidth - CONST.MAIN_WINDTH) / 2;
+    setFixed({
+        target: '.ir-paper-year-list',
+        scrollY: CONST.BANNER_HEIGHT - CONST.NAV_HEIGHT,
+        changedStyle: {
+            position: 'fixed',
+            left: $setFixedLeft,
+            top: CONST.SCROLL_Y
+        },
+        defaultStyle: {
+            position: 'absolute',
+            left: 0,
+            top: 0
+        }
     })
 })
