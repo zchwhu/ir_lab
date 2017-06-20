c2ae5382d80129c651dd3cfce0fb15e09c681a46
zchwhu <291053953@qq.com>
2017-06-21 01:29:24 +0800

    @dev Index page entry script

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 /**
  * Created by Administrator on 2017/6/20.
  */
+requirejs.config({
+    paths: {
+        'jquery': 'lib/jquery-1.7.1.min'
+    }
+});
+
+require(['jquery','js/util/slide.js','js/util/toggleShow.js'],function ($,slide,toggleShow) {
+    slide({
+        container: '.ir-control-group',
+        target: '.ir-team-list',
+        item: '.ir-team-item',
+        prev: '.prev',
+        next: '.next',
+        total: 4
+    })
+
+    toggleShow('.ir-user-logged','.ir-dropdown','show');
+})
