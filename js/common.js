73b217329fbbfc3c01720c69e9ac548448d5aa02
zchwhu <291053953@qq.com>
2017-06-21 01:28:36 +0800

    @dev Add scroll animation

--- js/common.js
+++ js/common.js
@@ -9,5 +9,18 @@ requirejs.config({
 });
 
 require(['jquery','js/util/toggleShow.js'],function ($,toggleShow) {
+    var CONST = {
+        NAV_HEIGHT: 100
+    }
+
     toggleShow('.ir-user-logged','.ir-dropdown','show');
+
+    $(window).scroll(function () {
+        var $scrollTop = $(document).scrollTop();
+        if($scrollTop>CONST.NAV_HEIGHT){
+            $(".ir-header").addClass('onscroll');
+        }else{
+            $(".ir-header").removeClass('onscroll');
+        }
+    });
 })
