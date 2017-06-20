7d6b06c2b93465d76ec5a4f70f0160afe3647dea
zchwhu <291053953@qq.com>
2017-06-21 01:28:16 +0800

    @dev Function slide

--- js/util/slide.js
+++ js/util/slide.js
@@ -9,26 +9,32 @@ requirejs.config({
 
 define(['jquery'], function ($) {
     function Slide(options) {
-        this.container = options.container;
+        this.container = $(options.container);
         this.total = options.total;
         this.target = $(options.target);
         this.item = $(options.item);
         this.prevControl = $(options.prev);
         this.nextControl = $(options.next);
+        this.speed = options.speed || 1000;
+        this.currentIndex = 0;
         this.init();
     }
 
     Slide.prototype.init = function () {
+        this.length = this.item.length;
+        if(this.item.length<=this.total){
+            return;
+        }
+        this.show();
         this.step = this.item.width();
+        this.target.width(this.item.length*this.step);
         this.bindEvents();
     }
 
     Slide.prototype.show = function () {
-        if(this.item.length>this.total){
-            this.container.css({
-                'visibility': 'visible'
-            })
-        }
+        this.container.css({
+            'visibility': 'visible'
+        })
     }
 
     Slide.prototype.bindEvents = function () {
@@ -40,16 +46,40 @@ define(['jquery'], function ($) {
         }.bind(this));
     }
 
-    Slide.prototype.slide = function () {
-
+    Slide.prototype.slide = function (dis) {
+        this.target.animate({
+            "marginLeft": "+=" + dis
+        },this.speed,"swing");
     }
 
     Slide.prototype.prev = function () {
-
+        if(this.target.is(":animated")){
+            return;
+        }
+        var dis;
+        if(this.currentIndex === 0){
+            this.currentIndex = this.length - this.total;
+            dis =  - this.step*(this.currentIndex);
+        }else{
+            dis = this.step;
+            this.currentIndex --;
+        }
+        this.slide(dis);
     }
 
-    slide.prototype.next = function () {
-
+    Slide.prototype.next = function () {
+        if(this.target.is(":animated")){
+            return;
+        }
+        var dis;
+        if(this.currentIndex+this.total === this.length){
+            dis = this.step * this.currentIndex;
+            this.currentIndex = 0;
+        }else{
+            dis = -this.step;
+            this.currentIndex++;
+        }
+        this.slide(dis);
     }
 
     return function (options) {
